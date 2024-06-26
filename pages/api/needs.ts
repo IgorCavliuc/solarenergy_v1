b4fc7a74ef08346260fc01a61936690bad0b0957
Cavliuc Igor <igor@tecorient.com>
2024-06-27 00:22:03 +0300

    fix

--- pages/api/needs.ts
+++ pages/api/needs.ts
@@ -106,11 +106,17 @@ export default async function handler(
     const data = calculateSolarPanelRequirements(input);
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+      tls: {
+        rejectUnauthorized: false,
+      },
+      connectionTimeout: 60000, // 60 seconds
     });
 
     const mailOptions = {
